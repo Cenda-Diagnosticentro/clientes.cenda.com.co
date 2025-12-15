88cf539376e8ca7e03358133f5bbbd0cc1e9a4d8
Sebastian Grisales <70991975+heysebas@users.noreply.github.com>
2025-12-15 11:17:22 -0500

    Update funcionariosService URL in config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.config = {
         // funcionariosService: 'https://api-funcionarios-admin-migracion-production.up.railway.app', // Base URL de funcionarios y gestión
 
 
-        funcionariosService: 'https://arm-pattern-framing-broadway.trycloudflare.com',
+        funcionariosService: 'https://wake-joe-desktop-fiction.trycloudflare.com',
         informesService: 'https://api-informes-rtm-production.up.railway.app',
     },
 
