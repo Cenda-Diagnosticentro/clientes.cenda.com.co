0c58b4fecc2d3718fd2a5cef418ea7691a6eb02c
JilmarV <veloza.jilma.2393@eam.edu.co>
2026-02-16 09:11:26 -0500

    UPDATE: Change funcionariosService and informesService URLs in config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // config.js
 window.config = {
     microservicios: {
-        funcionariosService: 'http://localhost:8082', // Base URL de funcionarios y gestión
-        informesService: 'http://localhost:8082', // Base URL de control de certificados de RTM
+        funcionariosService: 'http://localhost:8080', // Base URL de funcionarios y gestión
+        informesService: 'http://localhost:8080', // Base URL de control de certificados de RTM
         // informesService: 'https://api-informes-rtm-production.up.railway.app', // Base URL de control de certificados de RTM
         // funcionariosService: 'https://api-funcionarios-admin-migracion-production.up.railway.app', // Base URL de funcionarios y gestión
 
 
-        // funcionariosService: 'https://chemistry-cologne-larger-displaying.trycloudflare.com',
-        // informesService: 'https://api-informes-rtm-production.up.railway.app',
+        funcionariosService: 'https://chemistry-cologne-larger-displaying.trycloudflare.com',
+        informesService: 'https://api-informes-rtm-production.up.railway.app',
     },
 
     getServiceUrl: function (serviceName) {
